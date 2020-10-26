2def607dbb4b92bc8c27b3cb73d7f4d812941a7d
springland <fenglinmail@yahoo.com>
2020-10-25 20:08:17 -0400

    Initial commit

---
+++ inventory/src/test/java/com/springland365/bookstore/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springland365.bookstore.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order/src/test/java/com/springland365/bookstore/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springland365.bookstore.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
